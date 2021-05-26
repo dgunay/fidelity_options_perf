ef5398eb78e1915eb6f735ac82537abacac0d8f6
Devin Gunay <devingunay@gmail.com>
2021-05-26 00:39:18 -0700

    optional end date

--- fidelity_performance.py
+++ fidelity_performance.py
@@ -1,4 +1,4 @@
-from numpy import gradient
+from numpy import expand_dims, gradient
 from pandas.core.arrays.categorical import contains
 from pandas.core.frame import DataFrame
 import metrics
@@ -29,49 +29,56 @@ def match_group_1(rgx, string):
         return m.group(1)
     
     return None
+class Prototype:
+    def __init__(self, end_date=None):
+        self.end_date = pd.to_datetime(end_date)
 
-def prototype(csv_files: list):
-    # TODO: add stuff that filters the bs Fidelity adds to their exported CSVs
-    dfs = []
-    for file in csv_files:
-        df: DataFrame = pd.read_csv(file, index_col="Run Date")
-        dfs.append(df)
-    
-    df_merged = reduce(lambda  left,right: pd.merge(left,right,
-                                            how='outer'), dfs)
-    df_merged.sort_index()
-    # df = categorize_transactions(df_merged)
-    pd.set_option('display.max_columns', None)
-    # print(df)
+    def run(self, csv_files: list):
+        # TODO: add stuff that filters the bs Fidelity adds to their exported CSVs
+        dfs = []
+        for file in csv_files:
+            df: DataFrame = pd.read_csv(file, index_col="Run Date", parse_dates=["Run Date"], infer_datetime_format=True)
+            dfs.append(df)
 
-    # Tell me:
-    # - Gross revenue from sell to open options
-    # - Gross expense from buy to close options
-    # - which symbols I've netted the most on
-    symbols = df_merged['Action'].map(lambda s: match_group_1(r"\(([A-Z]+)\)", s)).dropna().unique()
-    total_rev = 0
-    total_expense = 0
-    for symbol in symbols:
-        contains_this_symbol = df_merged['Action'].str.contains(symbol)
-        sold_to_open = df_merged['Action'].str.contains('SOLD OPENING')
-        bought_to_close = df_merged['Action'].str.contains('BOUGHT CLOSING')
-        gross_revenue = df_merged[sold_to_open & contains_this_symbol]['Amount'].sum()
-        gross_expense = df_merged[bought_to_close & contains_this_symbol]['Amount'].sum()
-        total_rev += gross_revenue
-        total_expense += gross_expense
-        if gross_revenue or gross_expense:
-            print("-- {}: --".format(symbol))
-            print("Revenue from selling to open {}: ${}".format(symbol, gross_revenue))
-            print("Expense from buying to close {}: ${}".format(symbol, gross_expense))
+        df_merged = reduce(lambda  left,right: left.append(right), dfs)
+        df_merged =  df_merged.sort_index()
 
-    print("-- TOTAL --")
-    print("Gross revenue: ${}".format(total_rev))
-    print("Gross expense: ${}".format(total_expense))
-    print("Net: ${}".format(total_rev + total_expense))
+        if self.end_date:
+            df_merged = df_merged[:self.end_date]
+
+        # df = categorize_transactions(df_merged)
+        pd.set_option('display.max_columns', None)
+        pd.set_option('display.max_rows', None)
+        print(df_merged)
+
+        # Tell me:
+        # - Gross revenue from sell to open options
+        # - Gross expense from buy to close options
+        # - which symbols I've netted the most on
+        symbols = df_merged['Action'].map(lambda s: match_group_1(r"\(([A-Z]+)\)", s)).dropna().unique()
+        total_rev = 0
+        total_expense = 0
+        for symbol in symbols:
+            contains_this_symbol = df_merged['Action'].str.contains(symbol)
+            sold_to_open = df_merged['Action'].str.contains('SOLD OPENING')
+            bought_to_close = df_merged['Action'].str.contains('BOUGHT CLOSING')
+            gross_revenue = df_merged[sold_to_open & contains_this_symbol]['Amount'].sum()
+            gross_expense = df_merged[bought_to_close & contains_this_symbol]['Amount'].sum()
+            total_rev += gross_revenue
+            total_expense += gross_expense
+            if gross_revenue or gross_expense:
+                print("-- {}: --".format(symbol))
+                print("Revenue from selling to open {}: ${}".format(symbol, gross_revenue))
+                print("Expense from buying to close {}: ${}".format(symbol, gross_expense))
+
+        print("-- TOTAL --")
+        print("Gross revenue: ${}".format(total_rev))
+        print("Gross expense: ${}".format(total_expense))
+        print("Net: ${}".format(total_rev + total_expense))
 
 
 if __name__ == '__main__':
     # take cli args (files)
     fire.Fire(
-        prototype
+        Prototype
     )
